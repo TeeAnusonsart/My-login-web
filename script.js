3d17c779d52825f009313b440257357bc53b11bd
TeeAnusonsart <97290247+TeeAnusonsart@users.noreply.github.com>
2022-09-06 22:07:56 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+let openLogin = document.querySelector(".h1");
+let loginWrapper=document.querySelector(".login-wrapper");
+
+openLogin.addEventListener("click",()=>{
+    loginWrapper.classList.toggle("open");
+});
